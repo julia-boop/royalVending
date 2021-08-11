e520e279c8b28d5fcc97345aee0382bfab3a9820
julia-boop <crdr.jc@gmail.com>
2021-08-11 00:00:36 -0300

    test

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DB_USER,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": process.env.DB_USER_DEV,
+    "password": process.env.DB_PASS_DEV,
+    "database": process.env.DB_NAME_DEV,
+    "host": process.env.DB_HOST_DEV,
+    "port":8889,
     "dialect": "mysql"
   },
   "test": {
